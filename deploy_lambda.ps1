df65c3230eefcb1ae57fc0b01cdccea33620d0c2
banalna <judas.priest999@gmail.com>
2021-08-17 22:02:22 +0300

    update deploy_lambda.ps1

--- deploy_lambda.ps1
+++ deploy_lambda.ps1
@@ -11,10 +11,9 @@ if (Test-Path "dist") {
     
     $component = Get-Content -Path "component.json" | ConvertFrom-Json
     $functionName = "$($component.registry)/$($component.name):$($component.version)-function"
+    $createdFunctions = (aws lambda list-functions | ConvertFrom-Json).Functions
 
     Set-Location ./dist
-
-    $createdFunctions = (aws lambda list-functions | ConvertFrom-Json).Functions
 
     # Exist function flag 
     $isExist = $false
@@ -26,12 +25,17 @@ if (Test-Path "dist") {
         }
     }
 
+    # Update or create new lambda function
     if ($isExist) {
         aws lambda update-function-code --function-name $functionName --zip-file "fileb://$($component.name)-lambda-v$($component.version).zip"
     }
     else {
-        # aws lambda create-function --runtime nodejs14.x --function-name $functionName --zip-file "fileb://$($component.name)-lambda-v$($component.version).zip"
-        Write-Output "Lambda function is does not exist"
+        Write-Output "Lambda function is does not exist."
+        
+        $accountId = aws sts get-caller-identity --query Account --output text
+        $region = aws configure get region
+        
+        aws lambda create-function --runtime nodejs14.x --role "arn:aws:lambda:$($region):$($accountId):function:$($functionName)" --function-name $functionName --zip-file "fileb://$($component.name)-lambda-v$($component.version).zip"
     }
     
 }
