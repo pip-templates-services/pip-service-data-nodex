7ea2d20a696570840edef45b71e7eb362d7c4968
banalna <judas.priest999@gmail.com>
2021-09-02 20:17:43 +0300

    add prometheus to EntitiesLambdaFunction

--- src/containers/EntitiesLambdaFunction.ts
+++ src/containers/EntitiesLambdaFunction.ts
@@ -1,12 +1,14 @@
 /** @module container */
 
 import { LambdaFunction } from 'pip-services3-aws-nodex';
+import { DefaultPrometheusFactory } from 'pip-services3-prometheus-nodex';
 import { EntitiesServiceFactory } from '../build/EntitiesServiceFactory';
 
 export class EntitiesLambdaFunction extends LambdaFunction {
     public constructor() {
         super("pip-service-data", "Entities data microservice");
         this._factories.add(new EntitiesServiceFactory());
+        this._factories.add(new DefaultPrometheusFactory());
     }
 }
 
